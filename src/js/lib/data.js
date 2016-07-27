fcab17d4e862c593f67c2bb1defb6f6c7aadcb9b
Alex Windett <alex@bigfish.co.uk>
2016-07-27 21:11:39 +0100

    adding more seed data details

--- src/js/lib/data.js
+++ src/js/lib/data.js
@@ -2,121 +2,121 @@ export const defaultLocalData = [
     {
         "id": 1,
         "name": "https://css-tricks.com/",
-        "displayName": false
+        "displayName": "CSS Ticks"
     },
     {
         "id": 2,
         "name": "https://css-tricks.com/",
-        "displayName": false
+        "displayName": "CSS Ticks"
     },
     {
         "id": 3,
         "name": "http://www.bbc.co.uk/sport",
-        "displayName": false
+        "displayName": "BBC Sport"
     },
     {
         "id": 4,
         "name": "https://css-tricks.com/",
-        "displayName": false
+        "displayName": "CSS Ticks"
     },
     {
         "id": 5,
         "name": "http://www.bbc.co.uk/sport",
-        "displayName": false
+        "displayName": "BBC Sport"
     },
     {
         "id": 6,
         "name": "http://stackoverflow.com/",
-        "displayName": false
+        "displayName": "Stack Overflow"
     },
     {
         "id": 7,
         "name": "http://www.bbc.co.uk/sport",
-        "displayName": false
+        "displayName": "BBC Sport"
     },
     {
         "id": 8,
         "name": "http://stackoverflow.com/",
-        "displayName": false
+        "displayName": "Stack Overflow"
     },
     {
         "id": 9,
         "name": "http://www.bbc.co.uk/sport",
-        "displayName": false
+        "displayName": "BBC Sport"
     },
     {
         "id": 10,
         "name": "http://stackoverflow.com/",
-        "displayName": false
+        "displayName": "Stack Overflow"
     },
     {
         "id": 11,
         "name": "http://www.bbc.co.uk/sport",
-        "displayName": false
+        "displayName": "BBC Sport"
     },
     {
         "id": 12,
         "name": "http://stackoverflow.com/",
-        "displayName": false
+        "displayName": "Stack Overflow"
     },
     {
         "id": 13,
         "name": "https://css-tricks.com/",
-        "displayName": false
+        "displayName": "CSS Ticks"
     },
     {
         "id": 14,
         "name": "http://stackoverflow.com/",
-        "displayName": false
+        "displayName": "Stack Overflow"
     },
     {
         "id": 15,
         "name": "https://css-tricks.com/",
-        "displayName": false
+        "displayName": "CSS Ticks"
     },
     {
         "id": 16,
         "name": "http://stackoverflow.com/",
-        "displayName": false
+        "displayName": "Stack Overflow"
     },
     {
         "id": 17,
         "name": "https://css-tricks.com/",
-        "displayName": false
+        "displayName": "CSS Ticks"
     },
     {
         "id": 18,
         "name": "http://www.bbc.co.uk/sport",
-        "displayName": false
+        "displayName": "BBC Sport"
     },
     {
         "id": 19,
         "name": "http://stackoverflow.com/",
-        "displayName": false
+        "displayName": "Stack Overflow"
     },
     {
         "id": 20,
         "name": "http://www.bbc.co.uk/sport",
-        "displayName": false
+        "displayName": "BBC Sport"
     },
     {
         "id": 21,
         "name": "http://stackoverflow.com/",
-        "displayName": false
+        "displayName": "Stack Overflow"
     },
     {
         "id": 22,
         "name": "http://www.bbc.co.uk/sport",
-        "displayName": false
+        "displayName": "BBC Sport"
     },
     {
         "id": 23,
         "name": "http://stackoverflow.com/",
-        "displayName": false
+        "displayName": "Stack Overflow"
     },
     {
         "id": 24,
         "name": "http://www.bbc.co.uk/sport",
-        "displayName": false
+        "displayName": "BBC Sport"
     },
 ]
