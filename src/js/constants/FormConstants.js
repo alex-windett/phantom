08304728ee86aaeb4b6820553a08922ae7108c88
Alex Windett <alex@bigfish.co.uk>
2016-07-27 08:30:32 +0100

    deleting files

--- src/js/constants/FormConstants.js
+++
@@ -1,4 +0,0 @@
-export const cardNumberRegex    = /^(?:[0-9 ]+$)/
-export const dateLength         = 5 // MM/YY
-export const passwordLengthMin  = 8
-export const postcodeRegex      = /^[A-Z]{1,2}[0-9]{1,2}[A-Z]{0,1} *[0-9][A-Z]{2}$/i
