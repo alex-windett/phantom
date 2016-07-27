42d6339253f2bfe96201f24a145055aa45266d02
Alex Windett <alex@bigfish.co.uk>
2016-07-27 21:07:12 +0100

    adding in new sites to lib data

--- src/js/lib/data.js
+++ src/js/lib/data.js
@@ -1,122 +1,122 @@
 export const defaultLocalData = [
     {
         "id": 1,
-        "name": "http://phantom.alex.dev/",
+        "name": "https://css-tricks.com/",
         "displayName": false
     },
     {
         "id": 2,
-        "name": "http://phantom.alex.dev/",
+        "name": "https://css-tricks.com/",
         "displayName": false
     },
     {
         "id": 3,
-        "name": "http://phantom.alex.dev/",
+        "name": "http://www.bbc.co.uk/sport",
         "displayName": false
     },
     {
         "id": 4,
-        "name": "http://phantom.alex.dev/",
+        "name": "https://css-tricks.com/",
         "displayName": false
     },
     {
         "id": 5,
-        "name": "http://phantom.alex.dev/",
+        "name": "http://www.bbc.co.uk/sport",
         "displayName": false
     },
     {
         "id": 6,
-        "name": "http://phantom.alex.dev/",
+        "name": "http://stackoverflow.com/",
         "displayName": false
     },
     {
         "id": 7,
-        "name": "http://phantom.alex.dev/",
+        "name": "http://www.bbc.co.uk/sport",
         "displayName": false
     },
     {
         "id": 8,
-        "name": "http://phantom.alex.dev/",
+        "name": "http://stackoverflow.com/",
         "displayName": false
     },
     {
         "id": 9,
-        "name": "http://phantom.alex.dev/",
+        "name": "http://www.bbc.co.uk/sport",
         "displayName": false
     },
     {
         "id": 10,
-        "name": "http://phantom.alex.dev/",
+        "name": "http://stackoverflow.com/",
         "displayName": false
     },
     {
         "id": 11,
-        "name": "http://phantom.alex.dev/",
+        "name": "http://www.bbc.co.uk/sport",
         "displayName": false
     },
     {
         "id": 12,
-        "name": "http://phantom.alex.dev/",
+        "name": "http://stackoverflow.com/",
         "displayName": false
     },
     {
         "id": 13,
-        "name": "http://phantom.alex.dev/",
+        "name": "https://css-tricks.com/",
         "displayName": false
     },
     {
         "id": 14,
-        "name": "http://phantom.alex.dev/",
+        "name": "http://stackoverflow.com/",
         "displayName": false
     },
     {
         "id": 15,
-        "name": "http://phantom.alex.dev/",
+        "name": "https://css-tricks.com/",
         "displayName": false
     },
     {
         "id": 16,
-        "name": "http://phantom.alex.dev/",
+        "name": "http://stackoverflow.com/",
         "displayName": false
     },
     {
         "id": 17,
-        "name": "http://phantom.alex.dev/",
+        "name": "https://css-tricks.com/",
         "displayName": false
     },
     {
         "id": 18,
-        "name": "http://phantom.alex.dev/",
+        "name": "http://www.bbc.co.uk/sport",
         "displayName": false
     },
     {
         "id": 19,
-        "name": "http://phantom.alex.dev/",
+        "name": "http://stackoverflow.com/",
         "displayName": false
     },
     {
         "id": 20,
-        "name": "http://phantom.alex.dev/",
+        "name": "http://www.bbc.co.uk/sport",
         "displayName": false
     },
     {
         "id": 21,
-        "name": "http://phantom.alex.dev/",
+        "name": "http://stackoverflow.com/",
         "displayName": false
     },
     {
         "id": 22,
-        "name": "http://phantom.alex.dev/",
+        "name": "http://www.bbc.co.uk/sport",
         "displayName": false
     },
     {
         "id": 23,
-        "name": "http://phantom.alex.dev/",
+        "name": "http://stackoverflow.com/",
         "displayName": false
     },
     {
         "id": 24,
-        "name": "http://phantom.alex.dev/",
+        "name": "http://www.bbc.co.uk/sport",
         "displayName": false
     },
 ]
